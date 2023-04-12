3c7b1126c7c9d55017a5493390992d91972a3427
Filipe Monteiro <filipe.monteiro.br@avenuecode.com>
2023-04-12 17:27:55 -0300

    Remove Realtime Changes

--- src/stores/balance/actions.js
+++ src/stores/balance/actions.js
@@ -1,14 +1,6 @@
 import { onValue, ref } from "firebase/database";
-import Firebase from "src/boot/firebase";
 import Balance from "src/repositories/Finance/Balance";
 import Patrimony from "src/repositories/Finance/Patrimony";
-import { useAuthStore } from "stores/auth";
-
-const auth = useAuthStore();
-const userId = auth.getUser.uid; // Firebase.auth.currentUser.uid;
-const balancePath = `balances/${userId}`;
-const balancesRef = ref(Firebase.database, `${balancePath}`);
-const patrimonyRef = ref(Firebase.database, `patrimony/${userId}`);
 
 export default {
   add(object) {
@@ -17,24 +9,17 @@ export default {
   },
   async load() {
     this.loading = true;
+    this.list = []
 
-    if (auth.isLogged) {
-      this.list = []
+    // Read Balances
+    await Balance.fetch()
+      .then(response => this.list = response)
+      .catch((error) => console.error(`Error On Load User Balances`) && console.error(error));
 
-      // Read Balances
-      await Balance.fetch()
-        .then(response => this.list = response)
-        .catch((error) => console.error(`Error On Load User Balances`) && console.error(error));
-
-      // Read Patrimony
-      await Patrimony.fetch()
-        .then(value => this.patrimony = value)
-        .catch((error) => console.error(`Error On Load User Patrimony`) && console.error(error));
-
-      // Detect Changes
-      onValue(balancesRef, (snapshot) => this.list = Balance.parseBalancesObjToArray(snapshot.val()));
-      onValue(patrimonyRef, (snapshot) => this.patrimony = parseFloat(snapshot.val()));
-    }
+    // Read Patrimony
+    await Patrimony.fetch()
+      .then(value => this.patrimony = value)
+      .catch((error) => console.error(`Error On Load User Patrimony`) && console.error(error));
 
     this.loading = false;
   },
