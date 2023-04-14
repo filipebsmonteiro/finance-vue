d7f188cd01a84953c8f654ebcf754c7d78c52b7d
Filipe Monteiro <filipe.monteiro.br@avenuecode.com>
2023-04-14 01:20:46 -0300

    Fix Simulation

--- src/stores/projection/state.js
+++ src/stores/projection/state.js
@@ -1,18 +1,20 @@
 export default () => {
   return {
+    list: [],
+
     patrimony: 0,
     incomes: 0,
     costs: 0,
 
-    investment: 0.92, // Retorno mensal sobre o invetimento
-    inflation: 0, // Inflação mensal
-    investmentGrowth: 0,
-    costGrowth: 0,
+    investmentInPercent: 0,
+    costInPercent: 0,
 
-    month: {
-      max: 1440, // 120 Anos
-      simulator: 0,
-      independency: 0,
-    },
+    max: 1440, // 120 Anos
+
+    simulation: {
+      investment: 0.92,
+      inflation: 0,
+      months: 0
+    }
   };
 }
