44997a0fec7830458fef5778ffa75b834b91314f
Filipe Monteiro <filipebsmonteiro@gmail.com>
2024-07-05 13:29:06 -0300

    Remove Comments

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -3,8 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
