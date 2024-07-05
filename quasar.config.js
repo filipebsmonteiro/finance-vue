c35f24bede3c5c7d413ffac37cbe91b54fa6a12d
Filipe Monteiro <filipebsmonteiro@gmail.com>
2024-07-05 13:28:41 -0300

    Add dotenv configs

--- quasar.config.js
+++ quasar.config.js
@@ -61,6 +61,7 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      env: require('dotenv').config().parsed,
       target: {
         browser: ['esnext', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16'
--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -6,14 +6,14 @@ import { getDatabase } from "firebase/database";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDm0SCiDBAVjeuzVqHXPdvW-5LViiZqY4A",
-  authDomain: "finance-b1846.firebaseapp.com",
-  projectId: "finance-b1846",
-  storageBucket: "finance-b1846.appspot.com",
-  messagingSenderId: "673438932464",
-  appId: "1:673438932464:web:63a1ee9453a42b9b4d9135",
-  measurementId: "G-ZW5BVCFQYW",
-  databaseURL: "https://finance-b1846-default-rtdb.firebaseio.com",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 };
 
 // Initialize Firebase
